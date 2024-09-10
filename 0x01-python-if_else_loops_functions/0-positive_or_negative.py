0f57070d0e8411241db66ed9a2fc633e61fbbc23
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-10 10:09:48 -0700

    Print whether a random number is positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import random
-
 # This line assigns a random number to `number`
-number = random.randint(-10, 10) 
 
+number = random.randint(-10, 10)
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
