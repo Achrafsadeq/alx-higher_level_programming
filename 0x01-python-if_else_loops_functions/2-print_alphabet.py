f7d5fc572ad9a64efae5ef5f350da4319487cbda
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-10 10:24:28 -0700

    Print the ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 # Use a single print function and a loop to output the alphabet in lowercase
-print("".join(chr(i) for i in range(97, 123)), end="")
+print("{}".format("".join(chr(i) for i in range(97, 123))), end="")
