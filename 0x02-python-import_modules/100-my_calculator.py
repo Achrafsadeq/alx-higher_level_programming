d7440f5863cafe82c302c6aaeb80798998ce2cb9
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-12 11:15:06 -0700

    Build a program that handles basic arithmetic operations via command line

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,28 +4,15 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    # Verify that the correct number of command-line arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <num1> <operator> <num2>")
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    # Extract the command-line arguments
-    num1 = int(sys.argv[1])
-    op = sys.argv[2]
-    num2 = int(sys.argv[3])
-
-    # Determine and perform the requested arithmetic operation
-    if op == '+':
-        result = add(num1, num2)
-    elif op == '-':
-        result = sub(num1, num2)
-    elif op == '*':
-        result = mul(num1, num2)
-    elif op == '/':
-        result = div(num1, num2)
-    else:
-        print("Invalid operator. Supported operators: +, -, *, /")
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    # Output the computed result in the desired format
-    print(f"{num1} {op} {num2} = {result}")
+    operand1 = int(sys.argv[1])
+    operand2 = int(sys.argv[3])
+    print("{} {} {} = {}".format(operand1, sys.argv[2], operand2, ops[sys.argv[2]](operand1, operand2)))
