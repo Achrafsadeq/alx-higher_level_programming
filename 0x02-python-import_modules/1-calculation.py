63d3e6175790ce766c82cf975606f4b6fce82690
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-12 10:39:18 -0700

    Perform basic arithmetic operations using functions from another module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-# Ensure this block runs only when the script is executed directly
 if __name__ == "__main__":
     # Import all necessary functions from calculator_1.py
     from calculator_1 import add, sub, mul, div
 
-    a = 10  # Assign the value 10 to variable a
-    b = 5   # Assign the value 5 to variable b
-
+    a = 10
+    b = 5
     # Perform each operation and print the result
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
