0a9ecc34bf7f8a34fcb0934dd411897f08cda95d
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-12 10:44:25 -0700

    Print the number and list of arguments passed to a script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-
     argv = sys.argv
     argc = len(argv) - 1
-
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
