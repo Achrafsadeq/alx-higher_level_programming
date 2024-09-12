034d3a1e96a7ca294b2832013609760292f839fa
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-12 11:18:21 -0700

    Build a program that handles basic arithmetic operations via command line

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,15 +4,28 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
+    # Ensure correct number of arguments
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    # Extract and convert arguments
+    num1 = int(sys.argv[1])
+    op = sys.argv[2]
+    num2 = int(sys.argv[3])
+
+    # Calculate result based on operator
+    if op == '+':
+        result = add(num1, num2)
+    elif op == '-':
+        result = sub(num1, num2)
+    elif op == '*':
+        result = mul(num1, num2)
+    elif op == '/':
+        result = div(num1, num2)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    operand1 = int(sys.argv[1])
-    operand2 = int(sys.argv[3])
-    print("{} {} {} = {}".format(operand1, sys.argv[2], operand2, ops[sys.argv[2]](operand1, operand2)))
+    # Output the calculation
+    print(f"{num1} {op} {num2} = {result}")
