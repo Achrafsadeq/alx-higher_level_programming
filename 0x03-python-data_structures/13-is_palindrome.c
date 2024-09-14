0ceb46eceb7c091a3b5e3f90a04bed385deb33d0
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-14 14:39:09 -0700

    task 13: Check if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,10 +4,6 @@
  * reverse_listint - Reverses the order of nodes in a linked list.
  * @head: Address of the pointer to the head node of the list.
  *
- * This function iterates through the linked list, reversing the direction
- * of each node's pointer so that the list is reversed. The new head of the
- * list is pointed to by the original tail of the list.
- *
  * Return: The pointer to the new head of the reversed list.
  */
 void reverse_listint(listint_t **head)
@@ -31,16 +27,12 @@ void reverse_listint(listint_t **head)
  * is_palindrome - Determines if a linked list is a palindrome.
  * @head: Pointer to the pointer of the head node in the list.
  *
- * This function checks if the sequence of values in the linked list reads
- * the same forwards and backwards. It uses a two-pointer technique to 
- * find the middle of the list, reverses the second half, and compares it
- * with the first half to verify if it is a palindrome.
- *
  * Return: 1 if the list is a palindrome, otherwise 0.
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow_pointer = *head, *fast_pointer = *head, *temp_pointer = *head, *duplicate = NULL;
+	listint_t *slow_pointer = *head, *fast_pointer = *head,
+		  *temp_pointer = *head, *duplicate = NULL;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
