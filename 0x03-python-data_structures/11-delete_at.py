8317743889869aec28328cd15d23211f43e0d840
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-14 14:19:34 -0700

    task 11: Delete the item at a specific position in a list.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        my_list.pop(idx)
+def delete_at(my_list=[], index=0):
+    if 0 <= index < len(my_list):
+        my_list.pop(index)
     return my_list
