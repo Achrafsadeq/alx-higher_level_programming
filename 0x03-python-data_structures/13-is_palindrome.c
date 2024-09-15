b08df6bf93fa33b79fd78201f441e34448e72514
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-15 03:18:33 -0700

    task 13: Check if a singly linked list is a palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,76 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * reverse_listint - Reverses the order of nodes in a linked list.
+ * @head: Address of the pointer to the head node of the list.
+ *
+ * Return: The pointer to the new head of the reversed list.
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *previous = NULL;
+	listint_t *current_node = *head;
+	listint_t *next_node = NULL;
+
+	while (current_node)
+	{
+		next_node = current_node->next;
+		current_node->next = previous;
+		previous = current_node;
+		current_node = next_node;
+	}
+
+	*head = previous;
+}
+
+/**
+ * is_palindrome - Determines if a linked list is a palindrome.
+ * @head: Pointer to the pointer of the head node in the list.
+ *
+ * Return: 1 if the list is a palindrome, otherwise 0.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow_pointer = *head, *fast_pointer = *head,
+		  *temp_pointer = *head, *duplicate = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (1)
+	{
+		fast_pointer = fast_pointer->next->next;
+		if (!fast_pointer)
+		{
+			duplicate = slow_pointer->next;
+			break;
+		}
+		if (!fast_pointer->next)
+		{
+			duplicate = slow_pointer->next->next;
+			break;
+		}
+		slow_pointer = slow_pointer->next;
+	}
+
+	reverse_listint(&duplicate);
+
+	while (duplicate && temp_pointer)
+	{
+		if (temp_pointer->n == duplicate->n)
+		{
+			duplicate = duplicate->next;
+			temp_pointer = temp_pointer->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!duplicate)
+		return (1);
+
+	return (0);
+}
