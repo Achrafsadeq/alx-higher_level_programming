180ec71046466576c7ff66bd1535981d6f3f8d73
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-18 06:35:25 -0700

    task10: Return the key with the largest integer value for ALX System Engineering and DevOps.

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """Find and return the key with the highest integer value."""
+    if not a_dictionary:
+        return None
+
+    highest_key = None
+    highest_value = float('-inf')
+
+    for key in a_dictionary:
+        if a_dictionary[key] > highest_value:
+            highest_value = a_dictionary[key]
+            highest_key = key
+
+    return highest_key
