3e5aa8c21e719418e5f87219dec18220b743d636
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-18 11:19:06 -0700

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,17 +1,14 @@
 #include <Python.h>
 #include <stdio.h>
 
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-
 /**
- * print_python_list - Prints basic info about Python lists.
- * @p: A PyObject list.
+ * print_python_list - Prints basic info about Python lists
+ * @p: PyObject list
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, i;
-	PyObject *item;
+	Py_ssize_t size, allocated, i;
+	const char *type_name;
 
 	if (!PyList_Check(p))
 	{
@@ -20,32 +17,33 @@ void print_python_list(PyObject *p)
 	}
 
 	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+	printf("[*] Allocated = %zd\n", allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
-
+		PyObject *item = PyList_GetItem(p, i);
+		type_name = item->ob_type->tp_name;
+		printf("Element %zd: %s\n", i, type_name);
 		if (PyBytes_Check(item))
 			print_python_bytes(item);
 	}
 }
 
 /**
- * print_python_bytes - Prints basic info about Python bytes.
- * @p: A PyObject bytes object.
+ * print_python_bytes - Prints basic info about Python bytes objects
+ * @p: PyObject bytes object
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size;
-	Py_ssize_t i;
-	char *str;
+	Py_ssize_t size, i;
+	char *string;
+	int max_bytes;
 
 	printf("[.] bytes object info\n");
-
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
@@ -53,15 +51,16 @@ void print_python_bytes(PyObject *p)
 	}
 
 	size = PyBytes_Size(p);
-	str = PyBytes_AsString(p);
+	string = PyBytes_AsString(p);
+	max_bytes = size > 10 ? 10 : (int)size;
 
 	printf("  size: %zd\n", size);
-	printf("  trying string: %s\n", str);
-	printf("  first %zd bytes:", (size < 10 ? size + 1 : 10));
+	printf("  trying string: %s\n", string);
+	printf("  first %d bytes:", max_bytes + 1);
 
-	for (i = 0; i < (size < 10 ? size + 1 : 10); i++)
+	for (i = 0; i <= max_bytes && i < size; i++)
 	{
-		printf(" %02x", (unsigned char)str[i]);
+		printf(" %02x", (unsigned char)string[i]);
 	}
 	printf("\n");
 }
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python3
 
+
 def roman_to_int(roman_string):
-    """Converts a Roman numeral to an integer."""
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string or type(roman_string) != str:
         return 0
-
-    roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
-
     total = 0
-    length = len(roman_string)
-
-    for i in range(length):
-        # Get the value of the current Roman numeral
-        current_value = roman_dict.get(roman_string[i], 0)
-        
-        # If it's not the last numeral and the current one is smaller than the next, subtract
-        if i < length - 1 and current_value < roman_dict.get(roman_string[i + 1], 0):
-            total -= current_value
-        else:
-            total += current_value
-
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
     return total
-
