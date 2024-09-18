1d54717320f08a4e2c5224c0132ad74102ad2e63
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-18 13:56:10 -0700

    task16: Perform operations using the CPython API for Python dictionaries in ALX System Engineering and DevOps.

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,10 +2,10 @@
 #include <Python.h>
 
 /**
- * print_python_bytes - Outputs information about Python byte objects.
+ * print_python_bytes - Prints bytes information
  *
- * @p: Python object to be analyzed.
- * Return: Nothing is returned from this function.
+ * @p: Python Object
+ * Return: no return
  */
 void print_python_bytes(PyObject *p)
 {
@@ -18,34 +18,28 @@ void print_python_bytes(PyObject *p)
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-
 	size = ((PyVarObject *)(p))->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
-
 	printf("  size: %ld\n", size);
-	printf("  attempting to print string: %s\n", string);
-
+	printf("  trying string: %s\n", string);
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
-
 	printf("  first %ld bytes:", limit);
-
 	for (index = 0; index < limit; index++)
 		if (string[index] >= 0)
 			printf(" %02x", string[index]);
 		else
 			printf(" %02x", 256 + string[index]);
-
 	printf("\n");
 }
 
 /**
- * print_python_list - Displays details of a Python list object.
+ * print_python_list - Prints list information
  *
- * @p: Python object (list) to inspect.
- * Return: No value is returned.
+ * @p: Python Object
+ * Return: no return
  */
 void print_python_list(PyObject *p)
 {
@@ -55,11 +49,9 @@ void print_python_list(PyObject *p)
 
 	size = ((PyVarObject *)(p))->ob_size;
 	list = (PyListObject *)p;
-
 	printf("[*] Python list info\n");
-	printf("[*] Total size of the Python List = %ld\n", size);
-	printf("[*] Memory allocated = %ld\n", list->allocated);
-
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
 	for (index = 0; index < size; index++)
 	{
 		obj = ((PyListObject *)p)->ob_item[index];
