06376d9a0419c08041a347d4d62369dc93cfbe17
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-18 06:47:36 -0700

    task12: Convert a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
     """Converts a Roman numeral to an integer."""
     if not isinstance(roman_string, str) or roman_string is None:
@@ -16,7 +15,7 @@ def roman_to_int(roman_string):
     for i in range(length):
         # Get the value of the current Roman numeral
         current_value = roman_dict.get(roman_string[i], 0)
-        
+
         # If it's not the last numeral and the current one is smaller than the next, subtract
         if i < length - 1 and current_value < roman_dict.get(roman_string[i + 1], 0):
             total -= current_value
