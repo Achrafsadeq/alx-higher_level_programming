243f25fdc0f9f696b5fc2cab35df8babe25991ea
Achrafsadeq <achrafsadeqq@gmail.com>
2024-09-18 06:59:13 -0700

    task12: Convert a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    """Converts a Roman numeral to an integer."""
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_number = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_dict[roman_string[j]] > roman_dict[roman_string[j - 1]]:
-            roman_number += roman_dict[roman_string[j]] - 2 * \
-                        roman_dict[roman_string[j - 1]]
+
+    roman_dict = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+            }
+
+    total = 0
+    length = len(roman_string)
+
+    for i in range(length):
+        # Get the value of the current Roman numeral
+        current_value = roman_dict.get(roman_string[i], 0)
+
+        # If it's not the last numeral and the current one is smaller than the next, subtract
+        if i < length - 1 and current_value < roman_dict.get(roman_string[i + 1], 0):
+            total -= current_value
         else:
-            roman_number += roman_dict[roman_string[j]]
-    return roman_number
+            total += current_value
+
+            return total
