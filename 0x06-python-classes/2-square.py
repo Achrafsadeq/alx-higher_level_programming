fd80d33e7316863cc976097a91b5774fc932e83b
Achrafsadeq <achrafsadeqq@gmail.com>
2024-10-01 09:33:30 -0700

    task 2 : Add type and value verification for size. If size is not an integer, raise a TypeError, and if it's negative, raise a ValueError.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""A class that defines a square with a private size attribute."""
+"""Defines a class Square with size validation."""
 
 
 class Square:
-    """Represent a square."""
+    """A class that defines a square with a private size attribute."""
 
     def __init__(self, size=0):
         """Initialize a new Square.
