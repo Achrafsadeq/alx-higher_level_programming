d4fe07411f152518a582b00ec436deaca7b895e1
Achrafsadeq <achrafsadeqq@gmail.com>
2024-10-01 09:11:10 -0700

    task 0 : Write an empty class Square that defines a square.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+"""
+Module 0-square
+Defines an empty class Square.
+"""
+
+
 class Square:
-    """An empty class that defines a square."""
+    """
+    An empty class that defines a square.
+    """
     pass
