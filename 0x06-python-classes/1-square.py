fb323b462d7c3ac73d4d7684ee8e3cc23db3cefe
Achrafsadeq <achrafsadeqq@gmail.com>
2024-10-01 09:20:38 -0700

    task 1 : Define Square with a private instance attribute size, and no type or value verification.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 
-"""
-Module 1-square
-Defines a class Square with a private instance attribute size.
-"""
+"""Defines a class Square with a private instance attribute size."""
+
 
 class Square:
-    """
-    A class that defines a square with a private attribute for size.
-    """
+    """A class that defines a square with a private attribute for size."""
 
     def __init__(self, size):
-        """
-        Initialize the square with a private size attribute.
+        """Initialize a new Square.
 
         Args:
-            size (int): The size of the square.
+            size (int): The size of the new square.
         """
         self.__size = size
