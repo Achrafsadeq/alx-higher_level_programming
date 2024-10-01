da3dc603f07a9b319311d33e80746b4df7bfc22d
Achrafsadeq <achrafsadeqq@gmail.com>
2024-10-01 10:39:29 -0700

    task 100 : Write a method to return the square's string representation as a string that visually resembles the square. Override str.

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-"""defines a class Square that represents."""
+"""Define a class Square."""
 
 
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square object.
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
