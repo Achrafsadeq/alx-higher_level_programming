17bf18b88946a4c9decea77813f303769df7ad3a
Achrafsadeq <achrafsadeqq@gmail.com>
2024-11-20 10:00:39 -0800

    List shows by rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
--- List shows by rating sum
-SELECT tv_shows.title, SUM(tv_shows_rates.rate) AS rating
+-- List shows by total rating
+SELECT tv_shows.title, SUM(rate) AS rating
 FROM tv_shows
 JOIN tv_shows_rates ON tv_shows.id = tv_shows_rates.show_id
 GROUP BY tv_shows.title
