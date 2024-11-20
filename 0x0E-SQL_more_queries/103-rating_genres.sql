a121466ad52c232f9536f048768217cac30559d7
Achrafsadeq <achrafsadeqq@gmail.com>
2024-11-20 10:04:56 -0800

    List genres by rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,18 @@
--- List genres by rating sum
-SELECT tv_genres.name, SUM(tv_shows_rates.rate) AS rating
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_shows_rates ON tv_shows.id = tv_shows_rates.show_id
-GROUP BY tv_genres.name
-ORDER BY rating DESC;
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+-- 'tv_genres' contains genre names, 'tv_show_genres' links shows and genres
+-- 'tv_show_ratings' contains the rating data for each show
+-- First INNER JOIN connects genres with their shows
+-- Second INNER JOIN adds the ratings for those shows
+-- GROUP BY combines all ratings for each genre
+-- SUM calculates total rating points per genre
+-- Results sorted by total ratings in descending order
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
