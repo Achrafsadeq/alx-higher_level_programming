2e440dc437be2f9a69e478d04fe01fad7a272d2d
Achrafsadeq <achrafsadeqq@gmail.com>
2024-12-16 08:38:17 -0800

    Prints a square of size x (first argument) using X.

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
+
+const args = process.argv;
+const x = parseInt(args[2], 10);
 
 if (isNaN(x)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < x; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
