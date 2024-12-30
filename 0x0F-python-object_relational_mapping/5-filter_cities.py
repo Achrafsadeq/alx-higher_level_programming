774172b859079c4cb9c1fb67ffe9922b2a6e63db
Achrafsadeq <achrafsadeqq@gmail.com>
2024-12-30 07:59:04 -0800

    Lists all cities of a state given by user input.

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Lists all cities of a state from the database hbtn_0e_4_usa.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC;
+    """
+
+    cursor.execute(query, (state_name,))
+
+    results = cursor.fetchall()
+
+    print(", ".join(city[0] for city in results))
+
+    cursor.close()
+    db.close()
