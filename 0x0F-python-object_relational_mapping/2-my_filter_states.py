4d181919fe07555022a20807545a1da4eb758a54
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-03 04:53:46 -0800

    Displays states matching user input.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Script to display all values in the states tablei
-where name matches the argument."""
-
+"""Script to display all values in states table where name matches argument."""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -14,19 +12,13 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-
-    # Create a cursor object
     cursor = db.cursor()
-
-    # Use a parameterized query to prevent SQL injection
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (sys.argv[4],))
-
-    # Fetch and print the results
+    query = "SELECT * FROM states
+    WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(
+        sys.argv[4])
+    cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
         print(state)
-
-    # Close the cursor and database connection
     cursor.close()
     db.close()
