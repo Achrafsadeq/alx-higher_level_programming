0bf31288b460a8121ffb8aec93e14f8391b450d1
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-03 06:04:49 -0800

    Print all City objects from database.

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """
-Contains the class definition of City
+Defines the City class linked to the State class
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+from relationship_state import Base
+from sqlalchemy.orm import relationship
+
 
 class City(Base):
     """
-    Class definition for City, inherits from Base.
-    Links to the MySQL table 'cities'.
+    Represents a city in the database.
     """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
