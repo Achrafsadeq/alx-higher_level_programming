af198e28840dfc4606dfeb010456eb8ba6969f78
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-06 10:09:27 -0800

    Improve state/city models with relationships.

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -8,16 +8,13 @@ from relationship_city import City
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                          .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     new_state = State(name="California")
     new_state.cities = [City(name="San Francisco")]
     session.add(new_state)
     session.commit()
-
     session.close()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -7,11 +7,12 @@ from sqlalchemy.orm import relationship
 from relationship_city import Base, City
 
 
-
 class State(Base):
     """State class that inherits from Base"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan"
+    )
