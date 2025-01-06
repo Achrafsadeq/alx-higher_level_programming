cb0be1991b12191b969742b5dbaa90c1baad0b2c
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-06 08:01:18 -0800

    List all City objects with linked State names.

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -5,24 +5,18 @@ This script connects to a MySQL database, queries
 the `cities` and `states` tables,
 and prints the city ID, city name, and associated state name.
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                          .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-
     cities = session.query(City).order_by(City.id)
-
     for city in cities:
         print(f"{city.id}: {city.name} -> {city.state.name}")
-
     session.close()
