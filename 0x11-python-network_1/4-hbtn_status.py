2e4d2f328b2c49a67e9b926f81f87bdd1e09ed2b
Achrafsadeq <achrafsadeqq@gmail.com>
2025-01-17 07:52:37 -0800

    Write a Python script that fetches using requests.

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+using the requests package.
+This script sends a GET request to the specified URL
+and displays the body of the response.
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    # Send a GET request to the specified URL
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"    - type: {type(response.text)}")
+    print(f"    - content: {response.text}")
